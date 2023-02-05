5370a45769f1ef2a51f0f2cbd794ef47db501596
QiuXiuhao <3080961291@qq.com>
2023-02-06 00:14:50 +0800

    新增了登录页面

---
+++ pages/index/login.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<button>登录</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
