9f1ef64b8331ad3717341162b6a3841e15fc7b3d
QiuXiuhao <3080961291@qq.com>
2023-02-06 00:20:19 +0800

    新增了注册页面

---
+++ pages/index/register.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<button>注册</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
